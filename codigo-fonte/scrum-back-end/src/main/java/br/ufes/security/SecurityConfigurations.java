9bd5a250b4e4ac2d1d724f4b2657508a4e78254b
Pedro Henrique Robadel <ph.robadel@gmail.com>
2024-05-08 22:11:06 -0300

    Incluindo documentação OpenApi ao projeto

--- codigo-fonte/scrum-back-end/src/main/java/br/ufes/security/SecurityConfigurations.java
+++ codigo-fonte/scrum-back-end/src/main/java/br/ufes/security/SecurityConfigurations.java
@@ -1,6 +1,7 @@
 package br.ufes.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -21,6 +22,8 @@ public class SecurityConfigurations {
 	@Autowired
 	private SecurityFilter securityFilter;
 	
+	private static final String[] SWAGGER_PATHS = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/webjars/swagger-ui/**"};
+	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity.csrf(csrf -> csrf.disable())
@@ -29,6 +32,8 @@ public class SecurityConfigurations {
 						.requestMatchers(HttpMethod.GET, "/projetos").hasAuthority("SCRUM_MASTER")
 						.requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
 						.requestMatchers(HttpMethod.POST, "/login").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api-docs").permitAll()
+						.requestMatchers(SWAGGER_PATHS).permitAll()
 						.anyRequest().authenticated()
 				)
 				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
